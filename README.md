69256bf82f716f084933e8b8580ba2d02164f4c2
rtrippel <52794038+rtrippel@users.noreply.github.com>
2022-09-21 15:27:06 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# small-challenge-itemis
+## Problem 3: MERCHANT'S GUIDE TO THE GALAXY
---
+++ src/main/java/RunApplication.java
@@ -0,0 +1,5 @@
+public class RunApplication {
+    public static void main(String[] args) {
+        System.out.println("Welcome to \"MERCHANT'S GUIDE TO THE GALAXY\"");
+    }
+}
