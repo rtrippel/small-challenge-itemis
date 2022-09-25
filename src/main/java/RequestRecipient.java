14fd79ba3341030f35321a4009924d020f3ac75d
Roman Trippel <rtrippel84@gmail.com>
2022-09-25 14:50:09 +0300

    Added tests for Request Recipient when determining the cost of resources

--- src/main/java/RequestRecipient.java
+++ src/main/java/RequestRecipient.java
@@ -1,3 +1,5 @@
+import org.apache.commons.lang3.math.NumberUtils;
+
 import java.util.Scanner;
 
 public class RequestRecipient {
@@ -5,6 +7,47 @@ public class RequestRecipient {
     public String getRequest() {
         final Scanner scanner = new Scanner(System.in);
 
-        return scanner.nextLine();
+        return verifyRequest(scanner.nextLine());
+    }
+
+    private String verifyRequest(String request) {
+
+        if (isRequestContainsIs(request) &&
+                (isCalculationRequestCorrect(request)
+                        || isDeterminingRequestCorrect(request)
+                        || isInitializationRequestCorrect(request))) {
+            return request;
+        }
+
+        return null;
+    }
+
+    private boolean isRequestContainsIs(String request) {
+        return request.contains(" is ");
+    }
+
+    private boolean isCalculationRequestCorrect(String request) {
+        boolean isEndQuestionsMark = request.endsWith(" ?");
+        boolean isStartWithHowMuchOrManyCredits = request.startsWith("how much is ")
+                || request.startsWith("how many Credits is ");
+
+        return isEndQuestionsMark && isStartWithHowMuchOrManyCredits;
+    }
+
+    private boolean isDeterminingRequestCorrect(String request) {
+        boolean isEndCredits = request.endsWith(" Credits");
+        final String[] subStrings = request.split(" is ");
+        final String[] firstPartWords = subStrings[0].split(" ");
+        boolean isLeftPartCorrect =  firstPartWords.length > 1
+                && Character.isUpperCase(firstPartWords[firstPartWords.length -1].charAt(0));
+        final String secondPart = subStrings[1];
+        final String cost = secondPart.split(" ")[0];
+        boolean isNumber = NumberUtils.isCreatable(cost);
+        boolean isRightPartCorrect =  secondPart.split(" ").length == 2 && isEndCredits && isNumber;
+        return isLeftPartCorrect && isRightPartCorrect;
+    }
+
+    private boolean isInitializationRequestCorrect(String request) {
+        return request.split(" ").length == 3;
     }
 }
--- src/test/java/RequestRecipientTest.java
+++ src/test/java/RequestRecipientTest.java
@@ -1,11 +1,14 @@
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayInputStream;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class RequestRecipientTest {
 
@@ -25,15 +28,76 @@ public class RequestRecipientTest {
         );
     }
 
-
     @ParameterizedTest
     @MethodSource("provideRequestsFromTaskDescription")
     public void testGetRequestSuccess(String expected, String input) {
         System.setIn(new ByteArrayInputStream(input.getBytes()));
 
-        String result =  new RequestRecipient().getRequest();
+        String result = new RequestRecipient().getRequest();
 
         assertEquals(expected, result);
     }
 
+    @Test
+    public void testGetRequestWithoutIsFail() {
+        String incorrectRequest = "how much wood could a woodchuck chuck if a woodchuck could chuck wood ?";
+        System.setIn(new ByteArrayInputStream(incorrectRequest.getBytes()));
+
+        String result = new RequestRecipient().getRequest();
+
+        assertNull(result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "how many Credits is glob prok Silver",
+            "how much is pish tegj glob glob "})
+    public void testGetRequestWithoutQuestionMarkFail(String incorrectRequest) {
+        System.setIn(new ByteArrayInputStream(incorrectRequest.getBytes()));
+
+        String result = new RequestRecipient().getRequest();
+
+        assertNull(result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "prok is V ?",
+            "how is pish tegj glob glob ?"})
+    public void testGetRequestWhenDeterminingRequestWithQuestionMarkWithoutHowManyOrMuchFail(String incorrectRequest) {
+        System.setIn(new ByteArrayInputStream(incorrectRequest.getBytes()));
+
+        String result = new RequestRecipient().getRequest();
+
+        assertNull(result);
+    }
+
+    @Test
+    public void testGetRequestWhenDeterminingRequestAndCreditStartWithSmallLetter() {
+        String incorrectRequest = "glob prok Gold is 57800 credits";
+        System.setIn(new ByteArrayInputStream(incorrectRequest.getBytes()));
+
+        String result = new RequestRecipient().getRequest();
+
+        assertNull(result);
+    }
+
+    @Test
+    public void testGetRequestWhenDeterminingRequestAndResourcesNameStartWithSmallLetter() {
+        String incorrectRequest = "glob prok gold is 57800 Credits";
+        System.setIn(new ByteArrayInputStream(incorrectRequest.getBytes()));
+
+        String result = new RequestRecipient().getRequest();
+
+        assertNull(result);
+    }
+    @Test
+    public void testGetRequestWhenDeterminingWithoutCountOfResource() {
+        String incorrectRequest = "Silver is 34 Credits";
+        System.setIn(new ByteArrayInputStream(incorrectRequest.getBytes()));
+
+        String result = new RequestRecipient().getRequest();
+
+        assertNull(result);
+    }
 }
